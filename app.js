e17653f2374b69cd53f23926c595de57a6ae6493
OsmanOK <osman.okuyan@ogrenci.selcuk.edu.tr>
2019-01-19 01:59:16 +0300

    jsonwebtkon module deployed

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ const app = express();
 //db connection
 const db = require('./helper/db.js')();
 
+//config
+const config = require('./config');
+app.set('api_secret_key', config.api_secret_key);
+
+//Middleware
+const verifyToken = require('./middleware/verify-token');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -25,6 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api', verifyToken);
 app.use('/api/movies', movieRouter);
 app.use('/api/directors', directorRouter);
 
